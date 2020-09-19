33d28e37ed1309f2993f86add451995367360a71
stminjae <70407618+stminjae@users.noreply.github.com>
2020-09-18 22:28:51 -0500

    Removed the calendar button and moved the images side by side.

--- web/src/pages/Rent.jsx
+++ web/src/pages/Rent.jsx
@@ -5,48 +5,34 @@ class Rent extends React.Component {
     return (
       <div className="about-page">
         <h1>Rental</h1>
-        <p>Currently, we are offering rental services in the form of karaoke machines for personal use, and study spaces at our 
+        <p>Currently, we are offering rental services in the form of karaoke machines for personal use, and study spaces at our
 physical locations. To rent, please (insert text about how to rent each - via Google Form below, calling Paul, etc.)</p>
-        
-        <div className="row">
-              <div> 
-              <p>Study spaces  </p>
-              <p>Karaoke Machines </p>
 
-
-                <img src={require("../images/study.png")}/> 
-                
-
-                <img src={require("../images/mic 2.png")}/>
-
-              </div>
-
-
-              <p>Come study at Jaguar! Our rooms are now open for group study and meeting spaces.
-              </p>
-
-              <p>Bring the party to your home! Our karaoke machines are ready to rent. Delivery services available.
-              </p>
+        <div class="container">
+          <div class="pics_in_a_row">
+            <div class="img1"><h1>Karaoke machines</h1>
+            <img src={require("../images/mic 2.png")}/>
+            <p>Bring the party to your home! Our karaoke machines are ready to rent. Delivery services available.
+            </p>
+            </div>
+            <div class="img2"><h1>Study spaces</h1>
+            <img src={require("../images/study.png")}/>
+            <p>Come study at Jaguar! Our rooms are now open for group study and meeting spaces.
+            </p>
+            </div>
+         </div>
         </div>
 
-
-        <div className = "calendarButton">
-            <a href="/calendar"><button> View Calendar </button> </a>
-
-        </div>
-
-
-
-        <div> 
-
+        <div>
         <iframe className = "googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSfdohj3Fc-E2ZOyBijZ63-CisBN8OsdCyZjd0Nk20Z7ZeXmnA/viewform?embedded=true"   frameborder ='0' marginheight= '0' marginwidth= '0'>Loading…</iframe>
         </div>
-      
+
       </div>
 
-    );
 
-  }
-}
+            );
 
-export default Rent;
+          }
+          }
+
+          export default Rent;
