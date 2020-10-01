0fe8b3a21df8c9ff6005782d70e6892bae938d2f
Anderson Lam <anderson.lam14@gmail.com>
2020-10-01 13:23:53 -0700

    rent page

--- web/src/pages/Rent.jsx
+++ web/src/pages/Rent.jsx
@@ -8,18 +8,21 @@ class Rent extends React.Component {
         <p>Currently, we are offering rental services in the form of karaoke machines for personal use, and study spaces at our
 physical locations. To rent, please (insert text about how to rent each - via Google Form below, calling Paul, etc.)</p>
 
-        <div class="container">
-          <div class="pics_in_a_row">
+        <div class="container container-fluid">
+        <div className = "row">
+          <div class="col-4 pics_in_a_row">
             <div class="img1"><h1>Karaoke machines</h1>
             <img src={require("../images/mic 2.png")}/>
             <p>Bring the party to your home! Our karaoke machines are ready to rent. Delivery services available.
             </p>
             </div>
-            <div class="img2"><h1>Study spaces</h1>
+            <div class="col-4 img2"><h1>Study spaces</h1>
             <img src={require("../images/study.png")}/>
             <p>Come study at Jaguar! Our rooms are now open for group study and meeting spaces.
             </p>
             </div>
+         </div>
+
          </div>
         </div>
 
