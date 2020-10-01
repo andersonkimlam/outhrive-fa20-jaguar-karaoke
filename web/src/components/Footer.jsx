5e6e314ac3c3c62b68be5fdb3919dbcfb05f5715
Anderson Lam <anderson.lam14@gmail.com>
2020-10-01 14:07:34 -0700

    diff cols

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -17,7 +17,7 @@ class Footer extends React.Component {
         <div class="row2 m-row justify-content-between">
 
 
-        <div className ="Contacts col-4">
+        <div className ="Contacts col-sm-2 col-sm-push-4">
           <p>Contact us: (Paul's Phone #) <br/>
            <br/> <br/>
 
@@ -27,7 +27,7 @@ class Footer extends React.Component {
         </div>
 
 
-        <div className ="BerkeleyLocation col-4"  md={3} sm={12}>
+        <div className ="BerkeleyLocation col-sm-4 col-sm-push-2"  md={3} sm={12}>
                     <p><strong>Berkeley Location</strong> <br/> <br/>
 
                     <strong> Address </strong> &nbsp; &nbsp;2516 Durant Ave, Berkeley, CA 94704  <br/>
@@ -36,7 +36,7 @@ class Footer extends React.Component {
                     </p>
           </div>
 
-      <div className ="OaklandLocation col-4">
+      <div className ="OaklandLocation col-sm-2 col-sm-push-4">
 
                   <p><strong>Oakland Location</strong> <br/> <br/>
 
@@ -49,13 +49,13 @@ class Footer extends React.Component {
 
 
 
-          <div className ="fbdiv col-4"  md={3} sm={12}>
+          <div className ="fbdiv col-sm-4 col-sm-push-2"  md={3} sm={12}>
             <p><strong> Connect with us to stay updated!</strong></p>
            <iframe className = "fb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialjaguarkaraoke&tabs&width=340&height=120&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="120" border="none"overflow="hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 
           </div>
 
-          <div className ="gfmdiv col-4">
+          <div className ="gfmdiv col-sm-2 col-sm-push-4">
 
 
               <iframe className = "gofundme" src="https://www.gofundme.com/f/qz56ca-stop-jaguar-karaoke-from-closing-forever/widget/medium/support"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0' ></iframe>
