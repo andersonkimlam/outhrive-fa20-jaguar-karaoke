d5704954a0e0bc048db581ff771a2f0bd945ca4e
stminjae <70407618+stminjae@users.noreply.github.com>
2020-10-01 16:52:26 -0500

    Update Rent.jsx

--- web/src/pages/Rent.jsx
+++ web/src/pages/Rent.jsx
@@ -5,26 +5,26 @@ class Rent extends React.Component {
     return (
       <div className="about-page">
         <h1>Rental</h1>
-        <p>Currently, we are offering rental services in the form of karaoke machines for personal use, and study spaces at our
-physical locations. To rent, please (insert text about how to rent each - via Google Form below, calling Paul, etc.)</p>
+        <p>Do you miss singing your heart out? We are offering karaoke rentals delivered straight to your home. Start your party today by filling out the Google form below or call at 415-577-2196.</p>
 
         <div class="container">
           <div class="pics_in_a_row">
-            <div class="img1"><h1>Karaoke machines</h1>
+            <div class="img1"><h1>Karaoke Machines</h1>
+
             <img src={require("../images/mic 2.png")}/>
             <p>Bring the party to your home! Our karaoke machines are ready to rent. Delivery services available.
             </p>
             </div>
-            <div class="img2"><h1>Study spaces</h1>
+            <div class="img2"><h1>Study Spaces</h1>
             <img src={require("../images/study.png")}/>
-            <p>Come study at Jaguar! Our rooms are now open for group study and meeting spaces.
+            <p>Come back to our locations when we open rooms for group study and meeting spaces!
             </p>
             </div>
          </div>
         </div>
 
         <div>
-        <iframe className = "googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSfdohj3Fc-E2ZOyBijZ63-CisBN8OsdCyZjd0Nk20Z7ZeXmnA/viewform?embedded=true"   frameborder ='0' marginheight= '0' marginwidth= '0'>Loading…</iframe>
+        <iframe className = "googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSfdohj3Fc-E2ZOyBijZ63-CisBN8OsdCyZjd0Nk20Z7ZeXmnA/viewform?embedded=true" frameborder ='0' marginheight= '0' marginwidth= '0'>Loading…</iframe>
         </div>
 
       </div>
