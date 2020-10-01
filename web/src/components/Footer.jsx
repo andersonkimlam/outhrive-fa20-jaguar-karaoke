ebeb99b19c88416c614c547b2b46302c57f3e78c
Anderson Lam <anderson.lam14@gmail.com>
2020-10-01 13:13:19 -0700

    Small fixes

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -17,9 +17,9 @@ class Footer extends React.Component {
         <div class="row2 m-row justify-content-between">
 
 
-        <div className ="Contacts col-sm-4 col-sm-push-8">
+        <div className ="Contacts col-4">
           <p>Contact us: (Paul's Phone #) <br/>
-          Support us: (link to Gofundme) <br/> <br/>
+           <br/> <br/>
 
 
           © 2020 Jaguar Karaoke
@@ -27,54 +27,44 @@ class Footer extends React.Component {
         </div>
 
 
+        <div className ="BerkeleyLocation col-4"  md={3} sm={12}>
+                    <p><strong>Berkeley Location</strong> <br/> <br/>
 
+                    <strong> Address </strong> &nbsp; &nbsp;2516 Durant Ave, Berkeley, CA 94704  <br/>
+                    <strong> Hours </strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;Sun - Sat: 6PM - 2AM
 
-
-
-
-
-          <div className ="BerkeleyLocation col-sm-12 col-sm-push-12"  md={3} sm={12}>
-            <p><strong>Berkeley Location</strong> <br/> <br/>
-
-            <strong> Address </strong> &nbsp; &nbsp;2516 Durant Ave, Berkeley, CA 94704  <br/>
-            <strong> Hours </strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;Sun - Sat: 6PM - 2AM
-
-            </p>
+                    </p>
           </div>
 
-          <div className ="OaklandLocation col-sm-12 col-sm-push-12">
+      <div className ="OaklandLocation col-4">
 
-          <p><strong>Oakland Location</strong> <br/> <br/>
+                  <p><strong>Oakland Location</strong> <br/> <br/>
 
-          <strong> Address </strong> &nbsp; &nbsp;4390 Telegraph Ave, Oakland, CA 94609 <br/>
-          <strong> Hours </strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Sun - Sat: 6PM - 2AM
-          </p>
-          </div>
-
-
-          <div className ="SocialMedia col-sm-12 col-sm-push-12">
-          <p>
-            Follow us on social media! <br/>
-
-
-            <div className = "mediaImages">
-
-
-
-
-
-
-
- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialjaguarkaraoke&tabs&width=340&height=120&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="120" border="none"overflow="hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-
-
-    <iframe className = "gofundme" src="https://www.gofundme.com/f/qz56ca-stop-jaguar-karaoke-from-closing-forever/widget/medium/support"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0'></iframe>
-
-
-            </div>
-            </p>
-
+                  <strong> Address </strong> &nbsp; &nbsp;4390 Telegraph Ave, Oakland, CA 94609 <br/>
+                  <strong> Hours </strong> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; Sun - Sat: 6PM - 2AM
+                  </p>
         </div>
+
+
+
+
+
+          <div className ="fbdiv col-4"  md={3} sm={12}>
+            <p><strong> Connect with us to stay updated!</strong></p>
+           <iframe className = "fb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialjaguarkaraoke&tabs&width=340&height=120&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="120" border="none"overflow="hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+
+          </div>
+
+          <div className ="gfmdiv col-4">
+
+
+              <iframe className = "gofundme" src="https://www.gofundme.com/f/qz56ca-stop-jaguar-karaoke-from-closing-forever/widget/medium/support"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0' ></iframe>
+
+
+          </div>
+
+
+
 
 
 
--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -31,19 +31,19 @@ class Home extends React.Component{
               <br/>
               <iframe className = "map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3149.6809834049027!2d-122.26032924863442!3d37.86775427964323!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857c2f1441bb1d%3A0xc974342904e7c6f4!2sJaguar%20Karaoke%20Berkeley!5e0!3m2!1sen!2sus!4v1599936983018!5m2!1sen!2sus"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0'></iframe>
                 <div className="row">
-                  <p className="loc-bold">Address</p> <p className="loc-info">2516 Durant Ave, Berkeley, CA 94704</p>
+                  <p className="loc-bold">Address</p> <p className="loc-info">2516 Durant Ave, <br/> Berkeley, CA 94704</p>
                 </div>
                 <div className="row">
-                  <p className="loc-bold">Hours</p> <p className="loc-info">Sun - Sat: 6PM - 2AM</p>
+                  <p className="loc-bold">Hours</p> <p className="loc-info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sun - Sat: 6PM - 2AM</p>
                 </div>
               <h2 className="city">Oakland</h2>
               <br/>
               <iframe className = "map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.2073673137943!2d-122.26547704863519!3d37.83203097964974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857de257000673%3A0x34f03697e548dca5!2sJaguar%20Karaoke%20Oakland!5e0!3m2!1sen!2sus!4v1599937512604!5m2!1sen!2sus"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0'></iframe>
                 <div className="row">
-                  <p className="loc-bold">Address</p> <p className="loc-info">4390 Telegraph Ave, Oakland, CA 94609</p>
+                  <p className="loc-bold">Address</p> <p className="loc-info">4390 Telegraph Ave, <br/>Oakland, CA 94609</p>
                 </div>
                 <div className="row">
-                    <p className="loc-bold">Hours</p> <p className="loc-info">Sun - Sat: 6PM - 2AM</p>
+                    <p className="loc-bold">Hours</p> <p className="loc-info">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sun - Sat: 6PM - 2AM</p>
                   </div>
             </div>
 
