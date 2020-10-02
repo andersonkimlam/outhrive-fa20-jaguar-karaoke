df2229487d91588ca7e0c656a2b7820333f5188b
Anderson Lam <anderson.lam14@gmail.com>
2020-10-02 13:07:07 -0700

    test

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -18,19 +18,11 @@ class Footer extends React.Component {
 
 
         <div className ="Contacts col-sm-2 col-sm-push-4">
-<<<<<<< HEAD
-          <p>Contact us: (Paul's Phone #) <br/>
-           <br/> <br/> <br/>
-
-
-
-=======
           <p><strong>Contact us</strong><br/><br/>
            415-577-2196 <br/>
           </p>
           <p>
           © 2020 Jaguar Karaoke
->>>>>>> 1b14ad18ceaecfdb8036073149f10b3981988bf6
           </p>
 
         </div>
@@ -57,24 +49,10 @@ class Footer extends React.Component {
 
 
           <div className ="fbdiv col-sm-4 col-sm-push-2"  md={3} sm={12}>
-<<<<<<< HEAD
-            <p><strong> Connect with us to stay updated!</strong></p>
-           <iframe className = "fb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialjaguarkaraoke&tabs&width=340&height=120&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="120" border="none"overflow="hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-
-          </div>
-
-          <div className ="gfmdiv col-sm-2 col-sm-push-4">
-
-                    <p>  © 2020 Jaguar Karaoke </p>
-              <iframe className = "gofundme" src="https://www.gofundme.com/f/qz56ca-stop-jaguar-karaoke-from-closing-forever/widget/medium/support"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0' ></iframe>
-
-
-=======
             <p><strong> Connect with us</strong></p>
            <iframe className = "facebook" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fofficialjaguarkaraoke&tabs&width=340&height=120&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="100%" border="none"overflow="hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
-          
+
             <iframe className = "gofundme" src="https://www.gofundme.com/f/qz56ca-stop-jaguar-karaoke-from-closing-forever/widget/medium/support"   frameborder= '0' allowfullscreen= 'true' aria-hidden='false' tabindex= '0' ></iframe>
->>>>>>> 1b14ad18ceaecfdb8036073149f10b3981988bf6
           </div>
 
 
